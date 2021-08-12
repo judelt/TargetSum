a975788164481f5a90d20dea5b69c56439a7353b
judelt <mendez.puig@gmail.com>
2021-08-11 18:48:19 -0700

    shuffle random nums

--- src/components/App.js
+++ src/components/App.js
@@ -8,20 +8,22 @@
 
 import React from 'react';
 import Game from './Game';
-<script src="http://localhost:8097"></script>;
+import shuffle from 'lodash.shuffle';
 
 const App = () => {
   const randomNumberCount = 6;
   const randomNumbers = Array.from({length: randomNumberCount}).map(
     () => 1 + Math.floor(10 * Math.random()),
   );
+  const shuffledrandomNumbers = shuffle(randomNumbers)
   const initialSeconds = 10;
   console.log('render app');
   return (
     <Game
       randomNumberCount={randomNumberCount}
-      randomNumbers={randomNumbers}
+      shuffledrandomNumbers={shuffledrandomNumbers}
       initialSeconds={initialSeconds}
+      randomNumbers={randomNumbers}
     />
   );
 };
